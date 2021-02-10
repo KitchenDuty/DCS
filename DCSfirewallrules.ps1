5047774b0b8c83cab7c5a5b9f68975781c83936a
KitchenDuty <39544224+KitchenDuty@users.noreply.github.com>
2021-02-10 09:28:12 -0800

    Create DCSfirewallrules.ps1

---
+++ DCSfirewallrules.ps1
@@ -0,0 +1,20 @@
+# DCS TCP inbound rules
+New-NetFirewallRule -DisplayName "DCS TCP Inbound" -Direction Inbound -LocalPort 10308 -Protocol TCP -Action Allow
+
+# DCS UDP inbound rules
+New-NetFirewallRule -DisplayName "DCS UDP Inbound" -Direction Inbound -LocalPort 10308 -Protocol UDP -Action Allow
+
+# DCS WebGUI TCP inbound  rules
+New-NetFirewallRule -DisplayName "DCS WebGUI TCP Inbound" -Direction Inbound -LocalPort 8088 -Protocol TCP -Action Allow
+
+# TacView Realtime Telemetry
+New-NetFirewallRule -DisplayName "TacView Realtime Telemetry Inbound" -Direction Inbound -LocalPort 42674 -Protocol TCP -Action Allow
+
+# TacView Remote Control
+New-NetFirewallRule -DisplayName "TacView Remote Control Inbound" -Direction Inbound -LocalPort 42675 -Protocol TCP -Action Allow
+
+# DCS SRS TCP1
+New-NetFirewallRule -DisplayName "DCS SRS TCP1" -Direction Inbound -LocalPort 5002 -Protocol TCP -Action Allow
+
+# DCS SRS TCP2
+New-NetFirewallRule -DisplayName "DCS SRS TCP2" -Direction Inbound -LocalPort 5003 -Protocol TCP -Action Allow
