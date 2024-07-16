9a42f0cfc24ce62f425d3cc30090d2d90163535f
KitchenDuty <39544224+KitchenDuty@users.noreply.github.com>
2024-07-16 07:25:49 -0700

    Add files via upload

---
+++ remove_kneeboards_from_dcs.ps1
@@ -0,0 +1,27 @@
+﻿
+
+
+
+$dcs_install = "H:\games\DCS World OpenBeta\Mods\terrains\"
+
+
+#test location of install
+
+if (Test-path $dcs_install){
+#gets terrain folders
+    $map_folders = Get-ChildItem -Path $dcs_install -Directory -Force -ErrorAction SilentlyContinue | Select-Object FullName
+    }
+
+
+$maps =
+
+"H:\games\DCS World OpenBeta\Mods\terrains\Caucasus\kneeboard",      
+"H:\games\DCS World OpenBeta\Mods\terrains\Falklands\kneeboard",     
+"H:\games\DCS World OpenBeta\Mods\terrains\Kola\kneeboard",
+"H:\games\DCS World OpenBeta\Mods\terrains\MarianaIslands\kneeboard",
+"H:\games\DCS World OpenBeta\Mods\terrains\Nevada\kneeboard",           
+"H:\games\DCS World OpenBeta\Mods\terrains\Syria\kneeboard"
+
+foreach ($map in $maps){
+    Get-ChildItem -Path $map -Include * | remove-Item -recurse 
+}
